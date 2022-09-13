cc9c07692457d3f59e38744ade25307bc4b1cf10
Meri2233 <sharmamerika2@gmail.com>
2022-09-13 15:40:06 +0545

    completed

--- public/main.js
+++ public/main.js
@@ -1,5 +1,15 @@
 
 const videoEl = document.querySelector('.stream');
+const socket = io("http://localhost:7001");
+socket.on('connection', () => console.log("Connected to Server"));
+
+const peer = new Peer(undefined, {
+    host: "localhost",
+    port: 7001,
+    path: "/peerjs"
+})
+console.log(peer);
+
 
 navigator.mediaDevices.getUserMedia({
     video: true,
@@ -9,50 +19,53 @@ navigator.mediaDevices.getUserMedia({
         videoEl.play();
     })
 
-    peer.on('call',(call)=>{
+    peer.on('call', (call) => {
+        console.log("Call Received");
         call.answer(stream);
-    })
-
-    socket.on('user-add', (newPeerId) => {
-        let call = peer.call(newPeerId,stream);
-        call.on('stream',(remoteStream)=>{
+        call.on('stream', (remoteStream) => {
+            console.log("Stream Received")
             let newvideo = document.createElement('video');
             newvideo.srcObject = remoteStream;
             newvideo.addEventListener('loadedmetadata', () => {
                 newvideo.play();
-            })  
+            })
+            document.body.appendChild(newvideo)
+        })
+    })
+
+    
+    socket.on('user-add', (newPeerId) => {
+        let call = peer.call(newPeerId, stream);
+        call.on('stream', (remoteStream) => {
+            console.log("Stream Received")
+            let newvideo = document.createElement('video');
+            newvideo.srcObject = remoteStream;
+            newvideo.addEventListener('loadedmetadata', () => {
+                newvideo.play();
+            })
             document.body.appendChild(newvideo)
         })
         //var conn = peer.connect(newPeerId);
         // on open will be launch when you successfully connect to PeerServer   
         //conn.on('open', function () {
-            // here you have conn.id
+        // here you have conn.id
         //    conn.send('hi! from' + peer.id);
-       // });
-    })  
+        // });
+    })
 }).catch(e => console.log("Error Retrieving video"))
 
-const socket = io("http://localhost:7001");
 
-socket.on('connection', () => console.log("Connected to Server"));
-
-const peer = new Peer(undefined, {
-    host: "localhost",
-    port: 7001,
-    path: "/peerjs"
-});
-
-console.log(peer);
 
 peer.on('open', (id) => {
     console.log("Peer ID", id)
-    socket.emit('new-connection', peer.id);
+    setTimeout(() => {
+        socket.emit('new-connection', peer.id);
+    }, 500)
 })
 
 
 peer.on('connection', function (conn) {
     conn.on('data', function (data) {
-        // Will print 'hi!'
         console.log(data);
     });
 });
